ddff8d6fa1955040ec55d41e746ab716a519e518
Peyton Cysewski <pcysewski12@gmail.com>
2020-09-28 16:28:33 -0700

    added capitalize feature

---
+++ src/main/java/com/example/songr/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showHome(){
+        return "";
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello";
+    }
+    @GetMapping("/capitalize/{message}")
+    public String capitalize(Model model, @PathVariable String message) {
+        model.addAttribute("message", message.toUpperCase());
+        return "capitalize";
+    }
+}
