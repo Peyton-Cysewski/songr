29a2070eb91b3ebf199b1ea035bfd43c5ef96219
Peyton Cysewski <pcysewski12@gmail.com>
2020-09-29 23:23:44 -0700

    adding methods

--- src/main/java/com/example/songr/AlbumController.java
+++ src/main/java/com/example/songr/AlbumController.java
@@ -1,19 +1,29 @@
 package com.example.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 
 @Controller
 public class AlbumController {
+    @Autowired
+    public AlbumRepository albumRepository;
+
+    @PostMapping("/album")
+    public RedirectView addAlbum(String title, String artist, int songCount, int length, String imageUrl) {
+        Album album = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(album);
+        return new RedirectView("/albums");
+    }
+
     @GetMapping("/albums")
     public String showAlbums(Model model) {
-        ArrayList<Album> albumsList = new ArrayList();
-        albumsList.add(new Album("Album 1", "Artist 1", 10, 8099, "[album 1 URL]"));
-        albumsList.add(new Album("Album 2", "Artist 2", 15, 9751, "[album 2 URL]"));
-        albumsList.add(new Album("Album 3", "Artist 3", 17,10056, "[album 3 URL]"));
+        ArrayList<Album> albumsList = (ArrayList<Album>) albumRepository.findAll();
         model.addAttribute("albumsList", albumsList);
         return "albums";
     }
